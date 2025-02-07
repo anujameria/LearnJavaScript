ac5cbabb2e783af5822f836eb853a7c1d878a09c
anuj ameria <sainanuj28@gmail.com>
2025-02-07 10:09:11 +0530

    Learn JS

---
+++ Variable/variable.js
@@ -0,0 +1,8 @@
+let a = 50;
+const b =40;
+// let value change but const not change after declaretion
+// let ek function and block scope variable hai jo apne scope se bahar acces nahi hota
+
+console.log(a);
+console.log(b);
+
---
+++ loops.js
@@ -0,0 +1,43 @@
+// for (let index = 0; index <= 10; index++) {
+//     for (let i = 0; i <= 10; i++) {
+//         console.log(`table of ${index} is ${index*i}`);
+//         ;
+        
+//     }
+    
+// }
+
+//let myarry=[1,2,3,4,5]
+
+// for (const element of myarry) {
+//     console.log(element);
+// }
+
+// for (const key in myarry) {
+//     console.log(myarry[key]);
+    
+// }
+
+// myarry.forEach((item,index,Array)=>{
+//     console.log(item,index,Array);
+// })
+
+
+const myarry=[1,,2,3,4,5,6,7,8,9]
+
+// const newNum=myarry.filter((num)=>{
+//     return num >=5
+// })
+
+// console.log(newNum);
+
+
+// const sumArry=myarry.reduce((acc,value)=>{
+//     console.log(`${acc} and ${value}`);
+    
+//     return acc + value
+// },0)
+// console.log(sumArry);
+
+
+
